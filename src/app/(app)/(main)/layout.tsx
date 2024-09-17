0ba68a3c1f3aefafa6cb037315a20f891f80f1d3
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-09-17 16:52:51 -0300

    refactor: layout title throughout app Split main layout into (app)/layout.tsx and (app)/(main)/layout.tsx Colors changes Keep current link button glowing

---
+++ src/app/(app)/(main)/layout.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+import signup_light from "@/assets/signup_light.png"
+import signup_dark from "@/assets/signup_dark.png"
+import users_light from "@/assets/users_light.png"
+import users_dark from "@/assets/users_dark.png"
+import { useTheme } from "next-themes"
+import { usePathname } from "next/navigation"
+
+const isSelectedPage = (path: string, expected: string, theme: string | undefined) => {
+    if (path.split('/')[1] !== expected) {
+        return "";
+    }
+
+    if (!theme) {
+        return "bg-[rgb(110,110,110)] pointer-events-none"
+    }
+
+    return theme === "dark" ? "bg-[rgb(110,110,110)] pointer-events-none" : "bg-[rgb(150,150,150)] pointer-events-none"
+}
+
+export default function HomeLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+
+    const { theme } = useTheme();
+    const pathname = usePathname()
+
+    return (
+        <div>
+            <nav className="nav flex flex-col space-y-2">
+                <Link href={"/signup"} className={`link ${isSelectedPage(pathname, "signup", theme)}`}>
+                    <Image
+                        id="signup_logo"
+                        src={theme === "dark" ? signup_dark : signup_light}
+                        alt="Sign Up an admin logo"
+                        className="link_logo"
+                    />
+                    <span>Sign up an admin</span>
+                </Link>
+                <Link href={"/users"} className={`link ${isSelectedPage(pathname, "users", theme)}`}>
+                    <Image
+                        id="users_logo"
+                        src={theme === "dark" ? users_dark : users_light}
+                        alt="View all users logo"
+                        className="link_logo"
+                    />
+                    <span>View all users</span>
+                </Link>
+            </nav>
+            <main id="main_content">{children}</main>
+        </div>
+    )
+}
