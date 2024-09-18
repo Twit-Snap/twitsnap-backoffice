ae85c93c918e1f4159882710e61140029b677ebf
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-09-18 20:30:46 -0300

    refactor: delete themedStyleClass function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,3 @@
-import { useTheme } from "next-themes";
-
 export function classNames(...classes: any) {
 	return classes.filter(Boolean).join(" ");
-}
-
-export function themedStyleClass(styles: any, def: string) {
-	const { resolvedTheme } = useTheme();
-	return `${styles[def]} ${resolvedTheme === "dark" ? styles["dark"] : ""}`;
 }
