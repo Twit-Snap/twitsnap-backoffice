93a21cae3e18afa5e7220b3112a8a504574a23e2
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-09-18 20:42:25 -0300

    refactor: dark theme as default

--- src/app/(app)/(main)/signup/page.tsx
+++ src/app/(app)/(main)/signup/page.tsx
@@ -6,7 +6,6 @@ import styles from "@/styles/signup/signup.module.css";
 import form_input from "@/styles/signup/input.module.css";
 import Image from "next/image";
 import logo from "@/assets/icon.ico";
-import { themedStyleClass } from "@/utils/utils";
 
 const usernameAtom = atom("");
 const emailAtom = atom("");
@@ -37,160 +36,104 @@ export default function SignUp() {
 			<form onSubmit={handleSubmit} className={`${form.form}`}>
 				<h1 className={form.title}>Sign up a new admin</h1>
 				<div className="space-y-8">
-					<div
-						className={`${themedStyleClass(
-							form_input,
-							"textbox"
-						)} text-gray-400`}
-					>
+					<div className={`${form_input.textbox} text-gray-400`}>
 						<input
-							className={themedStyleClass(form_input, "")}
 							id={form_input.input}
 							maxLength={INPUT_MAX_LENGTH}
-							onChange={(e: any) => setUserName(e.target.value)}
-						></input>
+							onChange={(e: any) =>
+								setUserName(e.target.value)
+							}></input>
 						<label
 							id={form_input.placeholder}
-							className={`text-gray-400" + ${themedStyleClass(
-								form_input,
-								""
-							)} + ${
+							className={`text-gray-400 ${
 								username.length == 0
 									? ""
-									: ` ${themedStyleClass(form_input, "fill")}`
-							}`}
-						>
+									: ` ${form_input.fill}`
+							}`}>
 							User name
 						</label>
 						<label
 							id={form_input.len}
-							className={`${themedStyleClass(form_input, "len")}`}
-						>{`${username.length} / ${INPUT_MAX_LENGTH}`}</label>
+							className={`${form_input.len}`}>{`${username.length} / ${INPUT_MAX_LENGTH}`}</label>
 					</div>
-					<div
-						className={`${themedStyleClass(
-							form_input,
-							"textbox"
-						)} text-gray-400`}
-					>
+					<div className={`${form_input.textbox} text-gray-400`}>
 						<input
-							className={themedStyleClass(form_input, "")}
 							id={form_input.input}
 							type="email"
 							maxLength={INPUT_MAX_LENGTH}
-							onChange={(e: any) => setEmail(e.target.value)}
-						></input>
+							onChange={(e: any) =>
+								setEmail(e.target.value)
+							}></input>
 						<label
 							id={form_input.placeholder}
-							className={`text-gray-400" + ${themedStyleClass(
-								form_input,
-								""
-							)} + ${
-								email.length == 0
-									? ""
-									: ` ${themedStyleClass(form_input, "fill")}`
-							}`}
-						>
+							className={`text-gray-400 ${
+								email.length == 0 ? "" : ` ${form_input.fill}`
+							}`}>
 							E-mail
 						</label>
 						<label
 							id={form_input.len}
-							className={`${themedStyleClass(form_input, "len")}`}
-						>{`${email.length} / ${INPUT_MAX_LENGTH}`}</label>
+							className={`${form_input.len}`}>{`${email.length} / ${INPUT_MAX_LENGTH}`}</label>
 					</div>
-					<div
-						className={`${themedStyleClass(
-							form_input,
-							"textbox"
-						)} text-gray-400`}
-					>
+					<div className={`${form_input.textbox} text-gray-400`}>
 						<input
-							className={themedStyleClass(form_input, "")}
 							id={form_input.input}
 							maxLength={INPUT_MAX_LENGTH}
-							onChange={(e: any) => setName(e.target.value)}
-						></input>
+							onChange={(e: any) =>
+								setName(e.target.value)
+							}></input>
 						<label
 							id={form_input.placeholder}
-							className={`text-gray-400" + ${themedStyleClass(
-								form_input,
-								""
-							)} + ${
-								name.length == 0
-									? ""
-									: ` ${themedStyleClass(form_input, "fill")}`
-							}`}
-						>
+							className={`text-gray-400 ${
+								name.length == 0 ? "" : ` ${form_input.fill}`
+							}`}>
 							Name
 						</label>
 						<label
 							id={form_input.len}
-							className={`${themedStyleClass(form_input, "len")}`}
-						>{`${name.length} / ${INPUT_MAX_LENGTH}`}</label>
+							className={`${form_input.len}`}>{`${name.length} / ${INPUT_MAX_LENGTH}`}</label>
 					</div>
-					<div
-						className={`${themedStyleClass(
-							form_input,
-							"textbox"
-						)} text-gray-400`}
-					>
+					<div className={`${form_input.textbox} text-gray-400`}>
 						<input
-							className={themedStyleClass(form_input, "")}
 							id={form_input.input}
 							type="password"
 							maxLength={INPUT_MAX_LENGTH}
-							onChange={(e: any) => setPassword(e.target.value)}
-						></input>
+							onChange={(e: any) =>
+								setPassword(e.target.value)
+							}></input>
 						<label
 							id={form_input.placeholder}
-							className={`text-gray-400" + ${themedStyleClass(
-								form_input,
-								""
-							)} + ${
+							className={`text-gray-400 ${
 								password.length == 0
 									? ""
-									: ` ${themedStyleClass(form_input, "fill")}`
-							}`}
-						>
+									: ` ${form_input.fill}`
+							}`}>
 							Password
 						</label>
 						<label
 							id={form_input.len}
-							className={`${themedStyleClass(form_input, "len")}`}
-						>{`${password.length} / ${INPUT_MAX_LENGTH}`}</label>
+							className={`${form_input.len}`}>{`${password.length} / ${INPUT_MAX_LENGTH}`}</label>
 					</div>
-					<div
-						className={`${themedStyleClass(
-							form_input,
-							"textbox"
-						)} text-gray-400`}
-					>
+					<div className={`${form_input.textbox} text-gray-400`}>
 						<input
-							className={themedStyleClass(form_input, "")}
 							id={form_input.input}
 							type="password"
 							maxLength={INPUT_MAX_LENGTH}
 							onChange={(e: any) =>
 								setPasswordAlt(e.target.value)
-							}
-						></input>
+							}></input>
 						<label
 							id={form_input.placeholder}
-							className={`text-gray-400" + ${themedStyleClass(
-								form_input,
-								""
-							)} + ${
+							className={`text-gray-400 ${
 								passwordAlt.length == 0
 									? ""
-									: ` ${themedStyleClass(form_input, "fill")}`
-							}`}
-						>
+									: ` ${form_input.fill}`
+							}`}>
 							Confirm password
 						</label>
 						<label
 							id={form_input.len}
-							className={`${themedStyleClass(form_input, "len")}`}
-						>{`${passwordAlt.length} / ${INPUT_MAX_LENGTH}`}</label>
+							className={`${form_input.len}`}>{`${passwordAlt.length} / ${INPUT_MAX_LENGTH}`}</label>
 					</div>
 				</div>
 				<button type="submit" className={`${form.submit} `}>
--- src/app/(app)/login/layout.tsx
+++ src/app/(app)/login/layout.tsx
@@ -2,39 +2,21 @@
 
 import Image from "next/image";
 import logo from "@/assets/icon.ico";
-import { useEffect, useState } from "react";
 import styles from "@/styles/login/layout.module.css";
-import { themedStyleClass } from "@/utils/utils";
 
 export default function HomeLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const [mounted, setMounted] = useState(false);
-
-	// Avoid Hydration Mismatch
-	useEffect(() => {
-		setMounted(true);
-	}, []);
-
-	if (!mounted) {
-		return null;
-	}
-
 	return (
 		<div>
 			<header
-				className={`${themedStyleClass(
-					styles,
-					"banner"
-				)} flex place-items-center `}
+				className={`${styles.banner} flex place-items-center `}
 				role="banner">
 				<Image src={logo} alt="TwitSnap logo" />
 			</header>
-			<main className={themedStyleClass(styles, "login")}>
-				{children}
-			</main>
+			<main className={styles.login}>{children}</main>
 		</div>
 	);
 }
