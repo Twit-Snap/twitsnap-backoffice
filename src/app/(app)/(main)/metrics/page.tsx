01343c801b27b0c31429543402a3bb9b3b5487fe
Tinchocw <mcwikla@fi.uba.ar>
2024-11-11 00:45:45 -0300

    feat: add dependencies

--- src/app/(app)/(main)/metrics/page.tsx
+++ src/app/(app)/(main)/metrics/page.tsx
@@ -5,11 +5,9 @@ import { Grid, Card, CardActionArea, CardContent, Typography, Box } from '@mui/m
 import { useRouter } from 'next/navigation';
 
 const chartOptions = [
-    { id: 1, name: 'Register Chart', path: 'register-chart', description: 'Shows amount of users register, with success rate and average time vs time' },
-    { id: 2, name: 'Login Chart', path: 'login-chart', description: 'Shows amount of successfully logins, failed login attempts and average time vs time ' },
-    { id: 3, name: 'Register with provider Chart', path: 'register-with-provider-chart', description: 'Muestra el número de registros con identidad federada.' },
-    { id: 4, name: 'Login with provider Chart ', path: 'login-with-provider-chart', description: 'Muestra el número de inicios de sesión a través de proveedores federados.' },
-    { id: 5, name: 'Blocked users Chart ', path: 'blocked-chart', description: 'Shows the number of blocked users.' },
+    { id: 1, name: 'Register Charts', path: 'register-chart', description: 'Shows amount of successful and failed registers attempts, registers with and without provider and average time' },
+    { id: 2, name: 'Login Charts', path: 'login-chart', description: 'Shows amount of successful and failed login attempts, logins with and without provider and average time' },
+    { id: 3, name: 'Blocked users Charts ', path: 'blocked-chart', description: 'Shows the number of blocked users.' },
     //{ id: 6, name: 'Radar Chart', path: '/radar-chart', description: 'Muestra métricas de actividad por zona geográfica.' },
 ];
 
@@ -23,7 +21,7 @@ const ChartList = () => {
     return (
         <Box p={3}>
             <Typography variant="h4" align="center" color='#b6b4b4' gutterBottom sx={{ marginBottom: 4 }} >
-                Aviable Graphics
+                Aviable Metrics
             </Typography>
             <Grid container spacing={3}>
                 {chartOptions.map((chart) => (
